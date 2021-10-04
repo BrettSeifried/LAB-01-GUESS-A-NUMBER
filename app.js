b62cd8e8f4b2d2541d5a17a5501800242407b2ae
Brett Seifried <seifriedbrett@gmail.com>
2021-10-04 14:43:16 -0700

    numGuesses going up everytime clicked

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const userGuess = document.getElementById('')
+// Inputs
+const userGuess = document.getElementById('input');
+const button = document.getElementById('submit');
+let randomNum = Math.floor(Math.random() * (20 - 0) + 1);
+
+let numGuesses = 0;
+
+
+// events
+button.addEventListener('click', () =>{
+  numGuesses++;
+  console.log(numGuesses);
+});
+
+
+

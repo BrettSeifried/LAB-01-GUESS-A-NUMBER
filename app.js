65ea957beec96b2725fc20d6570fa0f104b6a6e1
Brett Seifried <seifriedbrett@gmail.com>
2021-10-04 15:17:57 -0700

    included not working JS for random numbers

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 // Inputs
 const userGuess = document.getElementById('input');
 const button = document.getElementById('submit');
+const remain = document.getElementById('chanes');
 let randomNum = Math.floor(Math.random() * (20 - 0) + 1);
-
 let numGuesses = 0;
-
-
-
 
 // events
 button.addEventListener('click', () =>{
   numGuesses++;
-  console.log(userGuess);
-  // let result;
-  //   if (userGuess)
+  const guess = (Number(userGuess.value));
+  motivation.style.display = 'none';
+
+  let remain;
+  if(guess === randomNum){
+    remain = 'You got it!'
+  } else if (numGuesses === 4) {
+    remain= 'You are out of guesses';
+    button.style.display = 'none'
+  } else if (guess > randomNum) {
+    remain = 'Number too high!';
+  } else if (guess < randomNum) {
+    remain = 'Number too low'
+  }
+
 });
 
 

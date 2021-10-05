dac17d125c3631ed2633a3aba4a8cbe52d1023b6
Brett Seifried <seifriedbrett@gmail.com>
2021-10-05 10:43:21 -0700

    added cypress testing

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,19 @@
 
-// describe('Example Test', ()=>{
-//     before(()=>{
-//         cy.visit('/');
-//     });
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
 
-//     describe('HTML elements', ()=>{   
-//         it('contains the alchemy logo', ()=>{
-//             // cy.get('.main-logo').should('be.visible');
-//         });
-//     });
-// });
+    describe('HTML elements', ()=>{   
+        it('contains refresh button', ()=>{
+            cy.get('#restart').should('be.visible');
+        });
+    });
+
+    describe('click on the button subtracts remaining guesses', ()=>{
+        it('lower remaining guesses by one each time', () => {
+            cy.get('#submit').click();
+            cy.get('#result').should('be.visible');
+        });
+    });
+});
